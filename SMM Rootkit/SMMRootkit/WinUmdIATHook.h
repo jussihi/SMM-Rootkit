e2e63e9a13b11dc2e097a2f344a67c84c4afa0f5
Jussi Hietanen <jussi.hietanen@iki.fi>
2020-02-04 01:21:11 +0200

    Fix rest of format errors in WinUmd*

--- SMM Rootkit/SMMRootkit/WinUmdIATHook.h
+++ SMM Rootkit/SMMRootkit/WinUmdIATHook.h
@@ -1,6 +1,5 @@
-#ifndef __smmrootkit_cheat_rootkittest_h__
-#define __smmrootkit_cheat_rootkittest_h__
-
+#ifndef __smmrootkit_win_umd_iat_h__
+#define __smmrootkit_win_umd_iat_h__
 
 #include <Base.h>
 
@@ -9,34 +8,35 @@
 #include "serial.h"
 #include "Memory.h"
 
-
-typedef enum _WinUmdIATState {
+typedef enum _WinUmdIATState
+{
   NO_PROCESS,
   WAITING_EXECUTION,
   SUCCESS
 } WinUmdIATState;
 
 // struct shared with wx64_umd_exec_c.c
-typedef struct tdUMD_EXEC_CONTEXT_LIMITED {
-	INT64 CMPXCHG;
-	CHAR8 Status;
-	VOID* ProcessHandle;
-	struct {
-		UINT64 CloseHandle;
-		UINT64 CreateFileA;
-		UINT64 CreateProcessA;
-		UINT64 CreateThread;
-		UINT64 GetExitCodeProcess;
-		UINT64 ReadFile;
-		UINT64 WriteFile;
-		UINT64 LocalAlloc;
-	} fn;
+typedef struct _WinUmdIATCtx
+{
+  INT64 CMPXCHG;
+  CHAR8 Status;
+  VOID *ProcessHandle;
+  struct
+  {
+    UINT64 CloseHandle;
+    UINT64 CreateFileA;
+    UINT64 CreateProcessA;
+    UINT64 CreateThread;
+    UINT64 GetExitCodeProcess;
+    UINT64 ReadFile;
+    UINT64 WriteFile;
+    UINT64 LocalAlloc;
+  } fn;
   // These strings are used to transfer needed information
   // to the userspace process
-	CHAR8 ParamString1[100];
-	CHAR8 ParamString2[100];
-} UMD_EXEC_CONTEXT_LIMITED, *PUMD_EXEC_CONTEXT_LIMITED;
-
+  CHAR8 ParamString1[100];
+  CHAR8 ParamString2[100];
+} WinUmdIATCtxLimited;
 
 BOOLEAN WindowsUmdIATHook();
 
